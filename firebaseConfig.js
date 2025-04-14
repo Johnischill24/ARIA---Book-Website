ba80cc2efc1d9aebbd6d952823e83170e6db7769
Craig.Williams <craigjmwilliams@gmail.com>
2025-04-14 12:16:01 -0400

    data for the firebase set up

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCIP2XEJ7Yu4bVvqO_D1JlsJGNAytTnkvM",
+    authDomain: "aria-51e5e.firebaseapp.com",
+    projectId: "aria-51e5e",
+    storageBucket: "aria-51e5e.firebasestorage.app",
+    messagingSenderId: "603730679149",
+    appId: "1:603730679149:web:c3d5266bfc4c2a7de8b664",
+    measurementId: "G-WP5V2QDYZQ"
+  };
+
+export default firebaseConfig;
